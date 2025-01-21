7052419809f69a0dbb2461223ab5de75a5e7e5a8
Jean <dev.jeancotillo@gmail.com>
2025-01-20 21:04:50 -0300

    adicionando arquivo dummy-users com usuários fictícios

---
+++ 01-starting-project/src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Mochi',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Nimbus',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Sushi',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Astro',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Luna',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Whiskerloo',
+    avatar: 'user-6.jpg',
+  },
+];
